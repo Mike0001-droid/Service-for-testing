dea76689f06640e9005f56333659b7a472461d71
Mikhail <majorov-2004@list.ru>
2023-07-08 02:58:46 +0500

    Made GET-Request for Test

--- api/views.py
+++ api/views.py
@@ -3,24 +3,66 @@ from django.forms import model_to_dict
 from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import *
 from .models import *
-
-
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
-""" class UserAPIList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserAPIUpdate(generics.UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
- """
+    @action(methods=['get'], detail=True)
+    def test(self, request, pk=None):
+        tests = Test.objects.get(id=pk)
+        all_subtest = [
+            i.name for i in Subtest.objects.filter(
+                test_id = pk
+            )
+        ]
+        all_subtest_id = [
+            i.id for i in Subtest.objects.filter(
+                test_id = pk
+            )
+        ]
+        all_questions = [
+            i.name for i in Questions.objects.filter(
+                subtest_id__in = all_subtest_id
+            )
+        ]
+        all_questions_id = [
+            i.id for i in Questions.objects.filter(
+                subtest_id__in = all_subtest_id
+            )
+        ]
+        all_answers = [
+            i.name for i in Answers.objects.filter(
+                question_id__in = all_questions_id
+            )
+        ]
+        all_answers_id = [
+            i.id for i in Answers.objects.filter(
+                question_id__in = all_questions_id
+            )
+        ]
+        all_scales = set(
+            i.name for i in Scales.objects.filter(
+                answers_id__in = all_answers_id
+            )
+        )
+        all_scales_id = set(
+            i.id for i in Scales.objects.filter(
+                answers_id__in = all_answers_id
+            )
+        )
+        all_interpret = [
+            i.name for i in Interpretations.objects.filter(
+                scale_id__in = list(all_scales_id)
+            )
+        ]  
+        return Response({
+            'tests': tests.name,
+            'subtest': all_subtest,
+            'questions': all_questions,
+            'answers': all_answers,
+            'scales': all_scales,
+            'interpretation': all_interpret 
+        })
--- drf/urls.py
+++ drf/urls.py
@@ -3,12 +3,10 @@ from django.urls import path, include
 from api.views import *
 from rest_framework import routers
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-    """ path('api/v1/userlist/', UserViewSet.as_view({'get': 'list'})),
-    path('api/v1/userlist/<int:pk>/',  UserViewSet.as_view({'put': 'update'})) """,
+    path('api/v1/', include(router.urls))
 ]
