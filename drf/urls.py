ae3e2837b9bf41622a4cb4edac24c5c0454966f7
Mike0001-droid <majorov-2004@list.ru>
2023-07-12 22:40:56 +0500

    Repair JWT Auth

--- drf/urls.py
+++ drf/urls.py
@@ -8,12 +8,10 @@ router.register(r'user', TestViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # http://127.0.0.1:8000/api/v1/user/1/test/
     path('api/v1/', include(router.urls)),
     path('api/v1/test/', TestAPIListForUsers.as_view()),
     path('api/v1/testupdate/<int:pk>/', TestAPIUpdate.as_view()),
     path('api/v1/listusers/', UserAPIList.as_view()),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_refresh'),
-
+    path('api/v1/')
 ]
