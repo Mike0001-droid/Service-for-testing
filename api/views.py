85b070997b9697f98aa741996413f797d02a2607
Mike0001-droid <majorov-2004@list.ru>
2023-07-27 16:53:11 +0500

    Refactoring code

--- api/views.py
+++ api/views.py
@@ -11,68 +11,6 @@ from .models import *
 from .permissions import *
 
 
-class TestViewSet(viewsets.ModelViewSet):
-    queryset = Test.objects.all()
-    serializer_class = TestSerializer
-
-    @action(methods=['get'], detail=True)
-    def test(self, request, pk=None):
-        tests = Test.objects.get(id=pk)
-        all_subtest = [
-            i.name for i in Subtest.objects.filter(
-                test_id=pk
-            )
-        ]
-        all_subtest_id = [
-            i.id for i in Subtest.objects.filter(
-                test_id=pk
-            )
-        ]
-        all_questions = [
-            i.name for i in Questions.objects.filter(
-                subtest_id__in=all_subtest_id
-            )
-        ]
-        all_questions_id = [
-            i.id for i in Questions.objects.filter(
-                subtest_id__in=all_subtest_id
-            )
-        ]
-        all_answers = [
-            i.name for i in Answers.objects.filter(
-                question_id__in=all_questions_id
-            )
-        ]
-        all_answers_id = [
-            i.id for i in Answers.objects.filter(
-                question_id__in=all_questions_id
-            )
-        ]
-        all_scales = set(
-            i.name for i in Scales.objects.filter(
-                answers_id__in=all_answers_id
-            )
-        )
-        all_scales_id = set(
-            i.id for i in Scales.objects.filter(
-                answers_id__in=all_answers_id
-            )
-        )
-        all_interpret = [
-            i.name for i in Interpretations.objects.filter(
-                scale_id__in=list(all_scales_id)
-            )
-        ]
-        return Response({
-            'tests': tests.name,
-            'subtest': all_subtest,
-            'questions': all_questions,
-            'answers': all_answers,
-            'scales': all_scales,
-            'interpretation': all_interpret
-        })
-
-
 class TestAPIListForUsers(generics.ListCreateAPIView):
     queryset = Test.objects.filter(status="Опубликовано")
     serializer_class = TestSerializer
@@ -91,14 +29,6 @@ def test_list(request):
     return render(request, 'tests.html', context)
 
 
-def first_test(request):
-    return render(request, 'firsttest.html')
-
-
-def listests(request):
-    return render(request, 'listests.html')
-
-
 def index(request):
     return render(request, 'index.html')
 
@@ -113,3 +43,13 @@ def signup(request):
 
 def profile(request):
     return render(request, 'profile.html')
+
+
+def pass_the_test(request, pk):
+    test = get_object_or_404(Test, pk=pk)
+    if request.method == 'GET':
+        return render(
+            request,
+            'test_passing.html',
+            {'test': test}
+        )
--- drf/urls.py
+++ drf/urls.py
@@ -14,14 +14,12 @@ urlpatterns = [
     # http://127.0.0.1:8000/api/v1/user/1/test/?format=json
     path('api/v1/', include(router.urls)),
     path('api/v1/test/', TestAPIListForUsers.as_view()),
-    path('api/v1/testupdate/<int:pk>/', TestAPIUpdate.as_view()),
-    path('api/v1/listusers/', UserAPIList.as_view()),
+    path('api/v1/subtests/', SubtestAPIListForUsers.as_view()),
     path('', index, name='index'),
     path('login', login, name='login'),
     path('new_signup/', vyuha.SignUpView.as_view(), name='signup'),
     path('profile', profile, name='profile'),
-    # path('user-create/', views.UserCreate, name="user_create"),
-    path('tests/', views.test_list),
+    path('tests/', login_required(views.test_list)),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('passing/<int:pk>', pass_the_test, name='pass_the_test'),
 
