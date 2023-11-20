4e863d1cf73c980684a62065c24df256d15497fd
Mike0001-droid <majorov-2004@list.ru>
2023-11-20 22:04:16 +0500

    Изменил пути media

--- server/drf/urls.py
+++ server/drf/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
+#from django.conf import settings
+from drf import settings
 from django.contrib.auth.decorators import login_required
 from django.conf.urls.static import static
 
