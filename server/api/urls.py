646c897486827543b5cb7c4b9f591e6f3e86503f
Mike0001-droid <majorov-2004@list.ru>
2024-02-17 01:30:42 +0500

    Исправил названия запросов на получение и создание попытки

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,8 +2,7 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'answer', views.AnswerViewSet, basename='answer')
-router.register(r'summscore', views.SummScoreViewSet, basename='summscore')
+router.register(r'attempt', views.AttemptViewSet, basename='attempt')
 router.register(r'subtest', views.SubtestViewSet, basename='subtest')
 app_name = 'api'
 urlpatterns = router.urls
--- server/api/views.py
+++ server/api/views.py
@@ -20,9 +20,9 @@ class SubtestViewSet(ViewSet):
         serializer = SubtestSerializer(queryset, many=True)
         return Response(serializer.data)
     
-class AnswerViewSet(ViewSet):
+class AttemptViewSet(ViewSet):
     @action(detail=False, methods=['post'], schema=AttemptSchema())
-    def return_answer(self, request):
+    def create_attempt(self, request):
         test_id=request.data['test']
         patternAnswer_id=request.data['patternAnswer'],
         question_id=request.data['question'],
@@ -59,28 +59,38 @@ class AnswerViewSet(ViewSet):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-
-class SummScoreViewSet(ViewSet):
-    def retrieve(self, request, pk):
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='by_attempt_id/(?P<pk>[a-zA-Z0-9_]+)',
+        url_name='by-attempt',
+    )
+    def attemptbyid(self, request, pk):
+        #Создаём массив со всеми шкалами теста
         queryset = Attemption.objects.get(id=pk)
         all_scales = []
-        all_scales_id = set()
         for i in queryset.answer['answers']:
             answer_obj = Answer.objects.get(id=i)
-            all_scales_id.add(answer_obj.scale.id)
             all_scales.append({
                 'scale_id': answer_obj.scale.id,
                 'answer': i
             })
+
+        #Создаем массив словарей с айдишником шкалы и баллов по шкале
         scales_answer = []
         for k in all_scales:
             summ_score = sum(list(Answer.objects.filter(id=k['answer'], scale_id=k['scale_id']).values_list('score', flat=True)))
             scales_answer.append({'scale_id': k['scale_id'], 'scores': summ_score})
+
+        #Суммируем баллы относительно одинаковых шкал, данные храним так же в виде массива словарей
         key = lambda x: x['scale_id']
         scores_summ = [
-            {'scale_id': k, 'scores': sum(int(x['scores']) for x in g)} 
+            {'scale_id': k, 'scores': sum(x['scores'] for x in g)} 
             for k, g in itertools.groupby(sorted(scales_answer, key=key), key=key)
         ]
+
+        #Достаем объекты интерпретаций, относительно шкал и делаем выборку относительно суммы баллов по данной шкале
+        #Записи добавляем в массив "response" и отдаем пользователю в качестве результатов пройденного теста
         response = []
         for k in scores_summ:
             interp_obj = Interpretation.objects.filter(scale_id=k['scale_id']).values("start_score", "finish_score", "description")
