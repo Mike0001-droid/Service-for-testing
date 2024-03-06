b08b7e5ce4310393ae74f95460b337833a3e479d
Mike0001-droid <majorov-2004@list.ru>
2024-03-06 14:27:01 +0500

    откатил все настройки назад

--- server/drf/settings.py
+++ server/drf/settings.py
@@ -68,7 +68,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken'
     'django_filters',
     'rest_framework_simplejwt',
     'rest_framework',
