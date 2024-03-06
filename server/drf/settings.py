deb81c05074160ca623a640153429509a03853f4
Mike0001-droid <majorov-2004@list.ru>
2024-03-06 14:26:02 +0500

    добавил обратно

--- server/drf/settings.py
+++ server/drf/settings.py
@@ -188,6 +188,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
 
     ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
